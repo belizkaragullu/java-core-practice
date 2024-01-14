e669cd37744399c8c7a09c842ace5f7c4b03dd39
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-14 23:47:25 +0300

    update

---
+++ src/main/java/org/example/EqualsTostring/Main.java
@@ -0,0 +1,32 @@
+package org.example.EqualsTostring;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Person person = new Person("Beliz", "Karagullu", 23);
+        System.out.println(person); //toString metotu cagrılır
+
+        Person person2 = new Person("Beliz", "Karagullu", 23);
+
+        boolean isSame = person.equals(person2);
+        System.out.println(isSame);
+
+        OverridedPerson overridedPerson = new OverridedPerson("Beliz", "Karagullu", 23);
+        System.out.println(overridedPerson); //override edilmis metod
+
+        OverridedPerson overridedPerson2 = new OverridedPerson("Beliz", "Karagullu", 23);
+
+        isSame = overridedPerson.equals(overridedPerson2);
+        System.out.println(isSame);
+
+
+
+
+
+
+
+
+    }
+}
