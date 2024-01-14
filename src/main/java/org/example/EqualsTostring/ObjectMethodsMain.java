238c1d64fc27f679777e97572cd37c16142e20ca
belizkaragullu <beliz.karagullu@gmail.com>
2024-01-15 00:54:43 +0300

    main name fix

--- src/main/java/org/example/EqualsTostring/ObjectMethodsMain.java
+++ src/main/java/org/example/EqualsTostring/ObjectMethodsMain.java
@@ -2,7 +2,7 @@ package org.example.EqualsTostring;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class ObjectMethodsMain {
     public static void main(String[] args) {
 
         Person person = new Person("Beliz", "Karagullu", 23);
